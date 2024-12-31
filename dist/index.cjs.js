8d7117602d1448752adc62b0ce49eaca77d97807
unknown <gino.huang@kjsoft.com.tw>
2024-12-31 10:52:36 +0800

    1234

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1) + "我是條狗狗12314";
 
 exports.capitalize = capitalize;
