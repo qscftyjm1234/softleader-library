8ffdb4f6ffa2ee39863043822f085adbb8ed01e1
unknown <gino.huang@kjsoft.com.tw>
2024-12-31 10:25:10 +0800

    上傳

---
+++ dist/index.cjs.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+exports.capitalize = capitalize;
---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [resolve(), typescript()],
+};
